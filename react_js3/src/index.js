a02bf6b15a9fbcf10400993fc7cfdb82fed33c27
AnoopKumar <anoop237kumar1993@gmail.com>
2018-01-21 00:49:00 +0530

    using webback bundler

--- react_js3/src/index.js
+++ react_js3/src/index.js
@@ -1,4 +1,4 @@
-const style={backgroundColor:'#3bd76f',color:'#fff',padding:'20px',margin:0};
+const style={backgroundColor:'#3bd76f',color:'#ffffff',padding:'20px',margin:0};
 ReactDOM.render(
   <h1 style={style}>Welcome to React.js</h1>,
   document.getElementById('root')
