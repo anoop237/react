6743a4d660d644da2ca1665211fd4d0e864fbde7
AnoopKumar <anoop237kumar1993@gmail.com>
2018-01-20 19:43:38 +0530

    Babel static transpiling with babel-cli

---
+++ react_js3/dist/bundle.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var style = { backgroundColor: 'orange', color: '#fff',padding:'20px' };
+ReactDOM.render(React.createElement(
+  'h1',
+  { style: style },
+  'Hello World'
+), document.getElementById('root'));
---
+++ react_js3/src/index.js
@@ -0,0 +1,6 @@
+const style={backgroundColor:'orange',color:'#fff',padding:'20px'};
+ReactDOM.render(
+  <h1 style={style}>Hello World</h1>,
+  document.getElementById('root')
+);
+
