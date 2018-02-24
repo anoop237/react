7adf7d4f662e9826dd12128c04adaf4c26e9c858
AnoopKumar <anoop237kumar1993@gmail.com>
2018-02-25 03:48:06 +0530

    creating first reducers

--- react_js7/src/index.js
+++ react_js7/src/index.js
@@ -1,14 +1,14 @@
 import C from './constants'
-import {allSkiDays,goal} from './initialstate.json'
+import {goal} from './store/reducers'
 
+const state = 10
+const action ={
+    type:C.SET_GOAL,
+    payload:15
+}
+const nextState = goal(state,action)
 console.log(`
-ski day counter
-===============
-The goal is ${goal} days
-Initially there are ${allSkiDays.length} days in list
-
-Constants(actions)
------------------
-${Object.keys(C).join('\n')}
-
+ initial goal:${state}
+ action:${JSON.stringify(action)}
+ new goal:${nextState}
 `)
