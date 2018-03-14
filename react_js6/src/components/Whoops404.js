a0d34dca4df50821e94bf3a7ae7ee50582e86733
AnoopKumar <anoop237kumar1993@gmail.com>
2018-03-14 10:01:32 +0530

    react-router v4 implementation

--- react_js6/src/components/Whoops404.js
+++ react_js6/src/components/Whoops404.js
@@ -1,13 +1,5 @@
 import React from 'react'
 import MainMenu from './MainMenu'
-export const Left = ({children})=><div>
-            <MainMenu/>
-            {children}
-        </div>
-export const Right = ({children})=><div>
-            <MainMenu/>
-            {children}
-        </div>
 export const Whoops404 = ()=><div>
             <h5>Whoops....404 Page not found...</h5>
         </div>
